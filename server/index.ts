ff9462e1e4acfa2cf50be5c46bb7906c7fb7c652
edgarizagni31 <ochoaEdgar3108@gmail.com>
2023-03-31 15:06:16 -0500

    I added auth routes and called the function to connect to the db

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+import { initializeDatabase } from './config/mongoose';
+import routerAuth from './api/auth/routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+initializeDatabase();
+
+//global middlewares
+app.use(bodyParser.json());
+
+//routes
+app.use(routerAuth);
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
