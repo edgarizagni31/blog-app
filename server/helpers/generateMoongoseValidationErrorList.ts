7138bd3a902b9192f631fcabe44c7de9c7969817
edgarizagni31 <ochoaEdgar3108@gmail.com>
2023-04-01 11:51:34 -0500

    add instance parameter to the constructor

--- server/helpers/generateMoongoseValidationErrorList.ts
+++ server/helpers/generateMoongoseValidationErrorList.ts
@@ -1,12 +1,13 @@
 import { Error } from 'mongoose';
 import APIError from '../classes/APIError';
 
-export const generateMoongoseValidationErrorList = ({
-  errors,
-}: Error.ValidationError) => {
+export const generateMoongoseValidationErrorList = (
+  { errors }: Error.ValidationError,
+  instance: string
+) => {
   const keys = Object.keys(errors);
   const listError = keys.map((key) => {
-    const err = new APIError(`${key} field`, '/auth/signUp');
+    const err = new APIError(`${key} field`, instance);
 
     err.setType = 'validation error';
     err.setDetail = errors[key].message;
